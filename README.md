562f8743873c72b6e6b3badf07abb1fd1ff1df46
rchamot <radoslaw.chamot@schibsted.pl>
2015-10-21 16:26:01 +0200

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+`qsub -I -q plgrid -l nodes=2:ppn=12 -l walltime=02:00:00`
+ 
---
+++ Zad1/start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#PBS -l nodes=2:ppn=4
+#PBS -l walltime=10:30:33
+
+module load libs/boost/1.52.0
+pip install --user mpi4py
+module load mvapich2
+module load mpiexec
+
+mpiexec -np 3 Zad1/pingpong.py
